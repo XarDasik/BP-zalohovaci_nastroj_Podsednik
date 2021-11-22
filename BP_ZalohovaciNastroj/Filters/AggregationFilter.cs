fdada8420350fb2edabf7b638bea0f974df7daa0
Filip Podsedník <45125874+XarDasik@users.noreply.github.com>
2021-11-23 00:21:16 +0100

    Add files via upload

---
+++ BP_ZalohovaciNastroj/Filters/AggregationFilter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BP_ZalohovaciNastroj
+{
+    class AggregationFilter : Filter
+    {
+        private AggregationOperand Operator;
+        private Filter[] Filters;
+
+        public AggregationFilter(bool OperatorNOT,string Name, AggregationOperand Operator, Filter[] filters) : base(OperatorNOT, Name)
+        {
+            this.Operator = Operator;
+            this.Filters = filters;
+        }
+
+        public override bool Accepts(FileInfo file)
+        {
+            switch (Operator){
+            case AggregationOperand.AND:
+                return AcceptsViaAnd(file);
+            case AggregationOperand.OR:
+                return AcceptsViaOr(file);
+            ///case AggregationOperand.NOT:
+                //return acceptsViaNot(file);
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        
+        private bool AcceptsViaAnd(FileInfo file)
+        {
+            foreach (Filter filter in Filters)
+            {
+                if (filter.Accepts(file) == false)
+                    return false;
+            }
+            return true;
+        }
+        private bool AcceptsViaOr(FileInfo file)
+        {
+            foreach (Filter Filter in Filters)
+            {
+                if (Filter.Accepts(file) == true)
+                    return true;
+            }
+            return false;
+        }
+
+       /* private bool acceptsViaNot(FileInfo file)
+        {
+            foreach (Filter Filter in Filters)
+            {
+                if (Filter.Accepts(file) == true)
+                    return false;
+            }
+            return true;
+        }*/
+    }
+}
---
+++ BP_ZalohovaciNastroj/Project.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BP_ZalohovaciNastroj
+{
+    class Project
+    {
+        public string Path { get; set; }
+        public FilterManager FilterManager { get; set; }
+    }
+}
